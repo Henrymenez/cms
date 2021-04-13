22ff9a62b4ca918704c74ab7dbd6d12ed3b99cf8
Henrymenez <76209501+Henrymenez@users.noreply.github.com>
2021-04-13 10:18:34 +0100

    Revert "Revert "last commit""
    This reverts commit e6b053eaf27897df78ddd3df5c804d81eabd479c.

--- dashboard.php
+++ dashboard.php
@@ -1,7 +1,5 @@
 <?php
-// if(!isset($_SESSION['loggedin'])){
-//     header("Location:../login.php");
-// }
+
 include_once("lib/header.php");
 ?>
         <p>Dashboard</p> <br> <hr>
--- lib/processreset.php
+++ lib/processreset.php
@@ -2,8 +2,13 @@
 
 
 $errorCount = 0;
-
 $username = $_POST['username'] != "" ? $_POST['username'] : $errorCount++;
+$email = $_POST['email'] != "" ? $_POST['email'] : $errorCount++;
+$password = $_POST['password'] != "" ? $_POST['password'] : $errorCount++;
+
+$_SESSION['email'] = $email;
+$_SESSION['username'] = $username;
+
 
 
 if($errorCount > 0){
@@ -16,23 +21,46 @@ if($errorCount > 0){
     for ($counter = 0; $counter < $countAll ; $counter++) { 
         $currentUser = $allUsers[$counter];
         
-        if($currentUser == $username . ".json"){
-            print_r($currentUser);
-            die();
-            
+        if($currentUser == $email . ".json"){
+
             $userString = file_get_contents("../db/".$currentUser); 
-        
             $userObject = json_decode($userString);
             $passwordDB = $userObject->password;
+            $usernameDB = $userObject->username;
 
-            
+             if($username != $usernameDB){
+                $_SESSION["error"] = "Reset code incorrect!!";
+                header("Location:../reset.php");
+                       die();
 
+               }else{
+                for ($counter = 0; $counter < $countAll ; $counter++) { 
+                    $currentUser = $allUsers[$counter];
+                    if($currentUser == $email . ".json"){
+                        $userString = file_get_contents("../db/".$currentUser); 
+                     $userObject = json_decode($userString) ;
+                      $userObject->password = password_hash($password,PASSWORD_DEFAULT);
+                     
+                        unlink("../db/".$currentUser);
+                    
+        file_put_contents("../db/". $email .  ".json", json_encode($userObject));
+        $_SESSION["message"] = "Password reset Successful , Login Please";
+        header("Location:../login.php");
+        
+                   die();
+                    }
+                }
+                
+                 
 
-    }
-}
-
+                 
+               }
+             }
+                }
+                $_SESSION["error"] = "Invalid Email";
+                header("Location:../reset.php");
+                    die();
  }
-    
 
 
 ?>
